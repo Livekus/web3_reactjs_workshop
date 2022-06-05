66616dbb450ec6e27a5f0bb9908cd7d349825260
kitthichai <skifgies@gmail.com>
2022-06-05 15:45:00 +0700

    create simple project hardhat

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "70f7400ad3aaa93f"; }
+/******/ 		__webpack_require__.h = function() { return "59476aae190c3ae5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
